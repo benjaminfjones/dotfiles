4350c5d1072ec77676bc8e533fa404d3dfdf91de
Benjamin Jones <benjaminfjones@gmail.com>
2014-10-19 12:18:54 -0700

    create swap/ dir on boot

--- vim/boot.sh
+++ vim/boot.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 if [ ! -d bundle/vundle ]; then
-	git clone https://github.com/gmarik/vundle.git bundle/vundle
+    git clone https://github.com/gmarik/vundle.git bundle/vundle
 else
-	cd bundle/vundle
-	git pull
+    cd bundle/vundle
+    git pull
+fi
+
+if [ ! -d swap ]; then
+    mkdir swap
 fi
 
 vim +BundleInstall +qa
