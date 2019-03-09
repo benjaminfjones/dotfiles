1131feb88afcf65c91567f2b2dd7769ccb502872
Benjamin Jones <benjaminfjones@gmail.com>
2019-03-08 20:51:25 -0800

    Remove "simple" option to the boot script

--- boot.sh
+++ boot.sh
@@ -1,29 +1,8 @@
 #!/usr/bin/env bash
 #
-# Link dotfiles to $HOME, making backups if old ones exist. If invoked with -s
-# or --simple, file in the 'simple/' directory will be prefered over ones in
-# the root of the repository (or subdirs of).
+# Link dotfiles to $HOME, making backups if old ones exist.
 #
 set -e
-
-# parse command line args
-SIMPLE=""
-while [[ $# -ge 1 ]]; do
-    key="$1"
-
-    case "$key" in
-        -s|--simple)
-        echo "Simple installation requested."
-        SIMPLE="y"
-        shift
-        ;;
-
-        *)
-        echo "Unrecognized parameter: $key"
-        exit 1
-        ;;
-    esac
-done
 
 # get absolute path to dotfiles repo (assumed to be where this script lives,
 # but not neccessarily where it's being exec'd from)
@@ -46,6 +25,7 @@ MK_DOT="ghci gitconfig tmux.conf vim zsh vim/vimrc zsh/zshrc zsh/zshrc_alias"
 echo "Linking dotfiles"
 for file in $(echo $MK_DOT)
 do
+
     dest="$HOME/.$(basename $file)"
     if [ -h "$dest" ]; then
         echo "removing symlink $dest"
@@ -55,14 +35,11 @@ do
         mv "$dest" "$dest.OLD"
     fi
 
-    if [[ "$SIMPLE" == "y" && -f "$REPO_DIR/simple/$file" ]]; then
-        src="$REPO_DIR/simple/$file"  # link the simple version
-    else
-        src="$REPO_DIR/$file"  # link the regular version
-    fi
-
+    # link the regular version
+    src="$REPO_DIR/$file"
     echo "linking $src to $dest"
     ln -s "$src" "$dest"
+
 done
 
 echo "Booting vim..."
@@ -70,13 +47,8 @@ pushd $HOME/.vim > /dev/null
 zsh boot.sh
 popd > /dev/null
 
-if [[ "$SIMPLE" == "y" ]]; then
-    echo "Skipping oh-my-zsh and tmux-status..."
-else
-    echo "Cloning oh-my-zsh and tmux-status..."
-    clone_home oh-my-zsh   http://github.com/robbyrussell/oh-my-zsh.git
-    clone_home tmux-status http://github.com/benjaminfjones/tmux-status.git
-fi
+echo "Cloning oh-my-zsh"
+clone_home oh-my-zsh http://github.com/robbyrussell/oh-my-zsh.git
 
 echo
 echo "Installation done!"
