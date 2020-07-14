3f80373df7126f0473181368188e2fc72355d24a
Benjamin Jones <benjaminfjones@gmail.com>
2020-07-14 11:50:29 -0700

    Remove gitconfig from autolinking loop

--- boot.sh
+++ boot.sh
@@ -20,7 +20,7 @@ echo "dotfiles REPO_DIR = $REPO_DIR"
 # git reset --hard HEAD
 
 # link dotfiles
-MK_DOT="gitconfig tmux.conf zsh zsh/zshrc zsh/zshrc_alias alacritty.yml"
+MK_DOT="tmux.conf zsh zsh/zshrc zsh/zshrc_alias alacritty.yml"
 
 echo "Linking dotfiles..."
 for file in $(echo $MK_DOT)
